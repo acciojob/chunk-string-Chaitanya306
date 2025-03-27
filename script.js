45e467963d1d0f76c3f3b83f231cd904ca43a05c
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-27 12:25:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	size=Number(size)
+	const ans=[]
+	let tempStr=''
+	for(let a=0;a<str.length;a++){
+		tempstr+=str[a]
+		if(tempStr.length==size || a==str.length){
+			ans.push(tempStr)
+			tempStr=''
+		}
+	}
+	return ans
 }
 
 // Do not change the code below
